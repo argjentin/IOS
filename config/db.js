03a2d1c00571bb87fa62aca452ad30e82b400b97
argjentin <91720956+argjentin@users.noreply.github.com>
2024-01-13 09:33:55 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Utiliser la variable d'environnement pour la connexion
-const databaseUrl = process.env.DATABASE_PRIVATE_URL;
+const databaseUrl = process.env.DATABASE_URL;
 
 // Création d'une nouvelle instance Sequelize en utilisant l'URL de connexion
 const sequelize = new Sequelize(databaseUrl);
