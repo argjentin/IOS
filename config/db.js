d800078a944803fb5ef7b26160b8221e894f547f
argjentin <91720956+argjentin@users.noreply.github.com>
2024-01-13 09:26:02 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,9 @@
-const { Sequelize } = require("sequelize");
-// require dotenv
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
 
-const IOS_POSTGRES_USER = process.env.IOS_POSTGRES_USER;
-const IOS_POSTGRES_PASSWORD = process.env.IOS_POSTGRES_PASSWORD;
-const IOS_POSTGRES_DB = process.env.IOS_POSTGRES_DB;
-const IOS_POSTGRES_HOST = process.env.IOS_POSTGRES_HOST;
+// Utiliser la variable d'environnement pour la connexion
+const databaseUrl = process.env.DATABASE_PRIVATE_URL;
 
-const sequelize = new Sequelize(
-  IOS_POSTGRES_DB,
-  IOS_POSTGRES_USER,
-  IOS_POSTGRES_PASSWORD,
-  {
-    host: IOS_POSTGRES_HOST,
-    dialect: "postgres",
-  }
-);
+// Création d'une nouvelle instance Sequelize en utilisant l'URL de connexion
+const sequelize = new Sequelize(databaseUrl);
 
 module.exports = sequelize;
