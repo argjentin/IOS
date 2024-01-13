1f851dfec914d53ef26652f757885be62df62f5d
argjentin <91720956+argjentin@users.noreply.github.com>
2024-01-13 10:47:32 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -14,10 +14,10 @@ Company.hasMany(User);
 
 // Synchronisation des modèles avec la base de données
 sequelize
-  .sync({ force: true })
+  .sync()
   .then(() => {
     console.log("Modèles synchronisés avec la base de données !");
-    insertRandomUserData();
+    // insertRandomUserData();
   })
   .catch((error) => {
     console.error("Erreur lors de la synchronisation :", error);
