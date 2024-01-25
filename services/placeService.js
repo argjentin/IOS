9d3ecf7a366a0af644bf2e589c40bed27f852873
argjentin <91720956+argjentin@users.noreply.github.com>
2024-01-25 16:50:05 +0100

    Update placeService.js

--- services/placeService.js
+++ services/placeService.js
@@ -12,6 +12,17 @@ class PlaceService {
     return await Place.findByPk(id);
   }
 
+    // create
+  static async create(data) {
+    // if id = 0, then set it to null
+    if (data.id === 0) {
+      data.isNull = true;
+    } else {
+      data.id = parseInt(data.id);
+    }
+    return await Place.create(data);
+  }
+
   static async getAllUserByPlace(id) {
     return await User.findAll({
       where: {
